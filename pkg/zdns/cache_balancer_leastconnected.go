855f3a58d87bb2d20799905f095bf9deebf584ab
Ronald Doorn <rdoorn@schubergphilis.com>
2018-02-13 15:46:29 +0100

    fix locking

--- pkg/zdns/cache_balancer_leastconnected.go
+++ pkg/zdns/cache_balancer_leastconnected.go
@@ -1,5 +1,7 @@
 package zdns
 
+import "sync/atomic"
+
 // LeastConnected based loadbalancing interface for statistics
 type LeastConnected struct{ Records }
 
@@ -7,10 +9,11 @@ type LeastConnected struct{ Records }
 func (s LeastConnected) Less(i, j int) bool {
 	// Fallback to round robin if we have no connected values yet
 
-	if s.Records[i].Statistics.Connected == 0 {
-		return s.Records[i].Statistics.Requests < s.Records[j].Statistics.Requests
+	if atomic.LoadInt64(&s.Records[i].Statistics.Connected) == 0 {
+		//return s.Records[i].Statistics.Requests < s.Records[j].Statistics.Requests
+		return atomic.LoadInt64(&s.Records[i].Statistics.Requests) < atomic.LoadInt64(&s.Records[j].Statistics.Requests)
 
 	}
 
-	return s.Records[i].Statistics.Connected < s.Records[j].Statistics.Connected
+	return atomic.LoadInt64(&s.Records[i].Statistics.Connected) < atomic.LoadInt64(&s.Records[j].Statistics.Connected)
 }
